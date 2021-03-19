11d71a9c4f8376a9cc0d741a8c28871a09ba7ed6
Alberto Biancardi <fieldyn@gmail.com>
2021-03-19 00:30:52 -0500

    37

---
+++ src/components/Menu.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+    <div class="position-sticky pt-3">
+      <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">
+            Dashboard
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Menu",
+  setup() {
+    return {};
+  },
+});
+</script>
